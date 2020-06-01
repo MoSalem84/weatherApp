cbddf7ed3e0c9b8fed97834bcda2ea75bda3a3de
Mohamed Salem <66252709+MoSalem84@users.noreply.github.com>
2020-06-01 14:18:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const weather = {};
 weather.temperature = {
     unit : "celsius"
 }
-
+// App consts and var 
+const KELVIN = 273;    
+const key = "82005d27a116c2880c8f0fcb866998a0";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
